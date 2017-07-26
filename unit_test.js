41134d40179c61234fa055070d3cd35b244d72b9
Kseni Platova <kplatova@gmail.com>
2017-07-26 14:55:21 +0300

    update

--- unit_test.js
+++ unit_test.js
@@ -24,3 +24,49 @@ const sequenceSum = (begin, end) => {
 };
 
 export default sequenceSum;
+// END
+
+
+// Факториал с помощью итеративного процесса
+const factorial = (n) => {
+  const iter = (current, acc) => {
+    if (current === 0) {
+      return acc;
+    }
+    
+    return iter(current - 1, acc * current);
+  }
+
+  return iter(n, 1);
+}
+
+// Функция должна находить минимальный делитель переданного числа.
+const smallestDivisor = (num) => {
+  const iter = (acc) => {
+    if (acc > num / 2) {
+      return num;
+    }
+    if (num % acc === 0) {
+      return acc;
+    }
+    return iter(acc + 1);
+  };
+
+  return iter(2);
+};
+
+export default smallestDivisor;
+
+factorial(4);
+// Алгоритм
+// Задание начального состояния
+// Проверка окончания процесса
+// Получение нового состояния
+// Повторение второго шага
+
+
+Рекурсивный vs Итеративный
+
+РП проще записать
+РП не может быть эффективно реализован (отложенное вычисление, потребляет память)
+ИП эффективно оптимизируется (Оптимизация хвостовой рекурсии)
