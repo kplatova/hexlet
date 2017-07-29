4e3e34f2d72a1177874b01689739c606b394a188
Kseni Platova <kplatova@gmail.com>
2017-07-29 23:31:35 +0300

    Основы программирование -> Окружение

---
+++ environment.js
@@ -0,0 +1,38 @@
+import { length, toUpperCase } from './strings';
+
+const bigLettersCount = (str) => {
+
+  let result = 0;
+  for (let i = 0; i < length(str); i += 1) {
+    if (toUpperCase(str[i]) === str[i]) {
+      result += 1;
+    }
+  }
+
+  return result;
+};
+
+
+const compare = (first, second) => {
+  const firstCount = bigLettersCount(first);
+  const secondCount = bigLettersCount(second);
+
+  // BEGIN (write your solution here)
+  if (firstCount > secondCount) {
+    return 1;
+  } else if (firstCount < secondCount) {
+    return -1;
+  }
+
+  return 0;
+  // END
+};
+
+export const greaterThan = (first, second) =>
+  compare(first, second) === 1;
+
+export const lessThan = (first, second) =>
+  compare(first, second) === -1;
+
+export const isEqual = (first, second) =>
+  compare(first, second) === 0;
