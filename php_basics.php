66933a97d41d2c477292160d4dd2d95ac1f8ab21
Kseni Platova <kplatova@gmail.com>
2017-08-04 16:32:01 +0300

    Update php_basics.php

--- php_basics.php
+++ php_basics.php
@@ -28,3 +28,24 @@ function uniq(array $arr)
     
     return $resultArr;
 }
+
+// Функция wordsCount, которая считает количество одинаковых слов в предложении. 
+// Результатом функции является ассоциативный массив, в ключах которого слова из исходного текста, 
+// а значения это количество повторений.
+function wordsCount($sentence)
+{
+    $words = explode(" ", $sentence);
+    $result = [];
+    foreach ($words as $word) {
+        if (empty($word)) {
+            continue;
+        }
+        if (!array_key_exists($word, $result)) {
+            $result[$word] = 1;
+        } else {
+            $result[$word]++;
+        }
+    }
+
+    return $result;
+}
