24c3af531000bfe0c05e8856d5ff29e10375e64a
Kseni Platova <kplatova@gmail.com>
2017-08-08 07:50:49 +0300

    Update js_composite_data.js

--- js_composite_data.js
+++ js_composite_data.js
@@ -7,3 +7,23 @@ console.log(getY(point));
 console.log(point);
 console.log(toString(point));
 console.log(quadrant(point));
+
+
+// pairs
+export const makeSegment = (point1, point2) => cons(point1, point2);
+
+export const startSegment = segment => car(segment);
+
+export const endSegment = segment => cdr(segment);
+
+export const segmentToString = segment =>
+  `[${pointToString(startSegment(segment))}, ${pointToString(endSegment(segment))}]`;
+
+export const midpointSegment = (segment) => {
+  const startPoint = startSegment(segment);
+  const endPoint = endSegment(segment);
+  const x = (getX(startPoint) + getX(endPoint)) / 2;
+  const y = (getY(startPoint) + getY(endPoint)) / 2;
+
+  return makePoint(x, y);
+};
