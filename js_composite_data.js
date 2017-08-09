535bf84c2818178b912ed443c5255793471f10cc
Kseni Platova <kplatova@gmail.com>
2017-08-09 11:40:32 +0300

    Update js_composite_data.js

--- js_composite_data.js
+++ js_composite_data.js
@@ -27,3 +27,21 @@ export const midpointSegment = (segment) => {
 
   return makePoint(x, y);
 };
+
+// барьеры обстракции
+export const makeRectangle = (point, width, height) => cons(point, cons(width, height));
+
+export const startPoint = rectangle => car(rectangle);
+export const height = rectangle => cdr(cdr(rectangle));
+export const width = rectangle => car(cdr(rectangle));
+
+export const square = rectangle => height(rectangle) * width(rectangle);
+export const perimeter = rectangle => 2 * (height(rectangle) + width(rectangle));
+
+export const containsTheOrigin = (rectangle) => {
+  const point1 = startPoint(rectangle);
+  const point2 = makePoint(getX(point1) + width(rectangle), getY(point1) - height(rectangle));
+
+  return quadrant(point1) === 2 && quadrant(point2) === 4;
+};
+// END
