aed4a2c7e271d511ddec4c814ddb0522b89146b9
Kseni Platova <kplatova@gmail.com>
2017-08-11 18:31:48 +0300

    Update classes.php

--- classes.php
+++ classes.php
@@ -13,3 +13,33 @@ return array_reduce($cart->items, function ($sum, $item) {
     return $sum + $item->price;
 }, 0);
 
+// BEGIN
+  public function add(Item $item)
+  {
+    $this->items[] = $item;
+  }
+
+  public function count()
+  {
+    return count($this->items);
+  }
+
+  public function total()
+  {
+    return array_reduce($this->items, function ($sum, $item) {
+      return $sum + $item->price;
+    }, 0);
+  }
+
+  public function remove($id)
+  {
+    $this->items = array_values(array_filter($this->items, function (Item $item) use ($id) {
+      return $id !== $item->id;
+    }));
+  }
+
+  public function clear()
+  {
+    $this->items = [];
+  }
+  // END
