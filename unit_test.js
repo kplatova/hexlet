8e37c03c5c5b190c0d25ade1e35e53be3a28d1cb
Kseni Platova <kplatova@gmail.com>
2018-04-13 17:15:47 +0300

    Update unit_test.js

--- unit_test.js
+++ unit_test.js
@@ -124,6 +124,22 @@ export default (str) => {
   return result;
 };
 
+/**
+* Переворот числа
+**/
+const reverseInt = (num) => {
+  const numAsStr = String(Math.abs(num));
+  let reversedStr = '';
+  let i = length(numAsStr) - 1;
+
+  while (i >= 0) {
+    reversedStr += numAsStr[i];
+    i -= 1;
+  }
+  const reversedModule = Number(reversedStr);
+
+  return num > 0 ? reversedModule : -reversedModule;
+}
 
 // Алгоритм
 // Задание начального состояния
